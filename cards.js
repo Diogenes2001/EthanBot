01a5728a707fb5d52e927e2daadcadc8a4ce1f92
Ethan Zhang <y2788zha@edu.uwaterloo.ca>
2020-01-11 23:09:54 -0500

    added more to chat

--- cards.js
+++ cards.js
@@ -1,33 +1,52 @@
 const { ActivityHandler, CardFactory } = require("botbuilder");
-const ADAPTIVE_CARD_OPTIONS = {
+const ADAPTIVE_CARD_OPTIONS = Object.freeze({
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
   "type": "AdaptiveCard",
   "version": "1.0",
     "body" : [
-        {
-            type: "TextBlock",
-            size: "medium",
-            color: "alert",
-            spacing: "none"
-        }
     ],
     "actions": []
-};
+});
+
+function addBlock(activity, text){
+    let block = {
+        text: text,
+        type: "TextBlock",
+        size: "default",
+        spacing: "medium",
+        wrap: true
+    }
+    activity.body.push(block);
+    return activity;
+}
 
 function makeText(promptText, waitForResponse){
 
     
     let activity = ADAPTIVE_CARD_OPTIONS;
-    activity.awaitUserResponse = waitForResponse,
-    activity.body[0].text = promptText;
+    activity.actions.length = 0;
+    activity.body.length = 0;
+    /*while('\n' in promptText){
+        activity = addBlock(activity, promptText.substring(0, promptText.find('\n')))
+        promptText = promptText.substring(promptText.find('\n'));
+    }
+    activity = addBlock(activity, promptText);
+    */
+   for(i = 0; i < promptText.length; ++i){
+       activity = addBlock(activity, promptText[i]);
+   }
+    activity.awaitUserResponse = waitForResponse;
     return CardFactory.adaptiveCard(activity);
 }
 
 function makeButtons(promptText, options){
     let activity = ADAPTIVE_CARD_OPTIONS;
     activity.actions.length = 0;
-    activity.awaitUserResponse = true,
-    activity.body[0].text = promptText;
+    activity.body.length = 0;
+    activity.awaitUserResponse = true;
+    for(i = 0; i < promptText.length; ++i){
+        activity = addBlock(activity, promptText[i]);
+    }
     for (let option of options) {
         activity.actions.push({
             type: "Action.Submit",
